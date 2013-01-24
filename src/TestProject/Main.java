09f6730c24b1f3d2b37458dc443c2468ba565912
tim78317 <tim78317@10.30.29.138>
2013-01-23 20:41:42 -0600

    put code in

--- src/TestProject/Main.java
+++ src/TestProject/Main.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TestProject;
 
 /**
@@ -14,6 +11,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello world");
     }
 }
